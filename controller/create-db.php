efd3ac9ae6756a120b5182a23db1a7787f4a6ebb
Brandon Jimenez <jimenezchronic@yahoo.com>
2014-11-17 21:54:42 -0800

    Checked for connection errors Milestone:09

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,3 +2,15 @@
 
 require_once ("../model/database.php");
 $connection = new mysqli($host, $username, $password);
+
+if ($connection->connect_error) {
+    die(' Error (' . $connection->connect_error . ') '
+            . $mysqli->connect_error);
+            
+}
+ else{
+     echo"success" . $connection->host_info;
+ }
+ 
+ $connection ->close();
+
