c6a41104a479093b9d7013729c2ff82a97f9acd5
Brandon Jimenez <jimenezchronic@yahoo.com>
2014-11-19 13:37:09 -0800

    commented code information.

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,15 +1,15 @@
 <?php
-require_once (__DIR__ ."/../model/database.php");
-$connection = new mysqli($host, $username, $password);
+require_once (__DIR__ ."/../model/database.php"); //this code allows me to get out of this file to accept code from another file.
+$connection = new mysqli($host, $username, $password); //variable that connects to server.
 
 if ($connection->connect_error) {
     die(' Error:  (' . $connection->connect_error . ') '
-            . $mysqli->connect_error);       
+            . $mysqli->connect_error);       //checks my connection.
 }
  
 else{
-     echo"Success: " . $connection->host_info;
+     echo"Success: " . $connection->host_info; //echos "success to inform me that the connection was successful.
  }
  
- $connection ->close();
+ $connection ->close(); //closes my conecion.
 
--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
     <?php
 
-$host = "localhost";
-$username = "root";
-$password = "root";
-$database = "blog_db";
+$host = "localhost"; //variable that holds my localhost.
+$username = "root";  //variable that holds my username.
+$password = "root";  //variable that holds my password.
+$database = "blog_db"; //variable that holds my database.
 
