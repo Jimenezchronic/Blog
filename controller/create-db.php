153413933ee941ddcb674db1122634c179300bee
Brandon Jimenez <jimenezchronic@yahoo.com>
2014-11-20 14:05:20 -0800

    created my database Milestone:12

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,15 +1,27 @@
 <?php
-require_once (__DIR__ ."/../model/database.php"); //this code allows me to accept code from another file.
+
+require_once (__DIR__ . "/../model/database.php"); //this code allows me to accept code from another file.
 $connection = new mysqli($host, $username, $password); //variable that connects to server.
 
 if ($connection->connect_error) {
-    die(' Error:  (' . $connection->connect_error . ') '
-            . $mysqli->connect_error);       //checks my connection.
+    die(" Error: " . $connection->connect_error);
 }
- 
-else{
-     echo"Success: " . $connection->host_info; //echos "success to inform me that the connection was successful.
- }
- 
- $connection ->close(); //closes my conecion.
+
+$exist = $connection->select_db($database); //selecting a database.
+
+
+if (!$exist) {
+    $query = $connection->query("CREATE DATABASE $database"); //sending a query to the server to create a new database.
+
+    
+    if ($query) {
+        echo "Database created successfully" . $database; //checks if the database has been created or not.
+    }
+} else {
+    echo"database already exist";
+}
+
+
+
+$connection->close(); //closes my conecion.
 
