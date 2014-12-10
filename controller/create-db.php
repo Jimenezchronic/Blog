f5c0b6f28001e85bbf3912c9346c8e04363b3244
Brandon Jimenez <jimenezchronic@yahoo.com>
2014-12-10 13:57:18 -0800

    Ran my query and checked for existence Milestone:15

--- controller/create-db.php
+++ controller/create-db.php
@@ -15,13 +15,25 @@ if (!$exist) {
 
     
     if ($query) {
-        echo "Database created successfully" . $database; //checks if the database has been created or not.
+        echo "<p>Database created successfully" . $database. "</p>"; //checks if the database has been created or not.
     }
 } else {
-    echo"database already exist"; //echoeing that we already made a database.
+    echo"<p>database already exist</p>"; //echoeing that we already made a database.
 }
 
+$query = $connection -> query("CREATE TABLE posts ("
+        . "id int(11)NOT NULL AUTO_INCREMENT,"
+        . "title varchar(255) NOT NULL,"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))"); //created a table
 
+if($query){
+    echo "<p>successfully create table: Posts</p> ";
+}
+
+else{
+    echo"<p> $connection->error </p>"; 
+}
 
 $connection->close(); //closes my conecion.
 
