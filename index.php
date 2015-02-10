71697c1faeb179f0928557a4b0f6b7d4167f4ec9
Brandon Jimenez <jimenezchronic@yahoo.com>
2015-02-10 14:03:48 -0800

    ERROR!!!! it wont run it just errors out

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-     require_once (__DIR__ . "/controller/create-db.php "); //accesses the creat-db.php file.
-        ?>
-    </body>
-</html>
+ <?php
+ require_once (__DIR__ . "/view/header.php");
+ require_once (__DIR__ . "/view/footer.php");
+ require_once (__DIR__ . "/controller/create-db.php "); //accesses the creat-db.php file.
+?>
+
+    
